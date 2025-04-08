4c6b1b24d866bf2e8ffae5153ab3fbe3e42b6cd3
Byakuya91 <nganti91@yahoo.com>
2025-04-08 10:57:12 -0400

    I've worked on the display for the prime checker. I will move into functionality. A bit differently from how I operate but I wanted to get some CSS practice.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // TODO:
+  // 1Create clear for CSS and basic styles(ONGOING)
+  // 2.Create HTML skeleton for prime checker
+  // 3. Create state variables to hold the user input.
+  // 4. Create a function to check if the number is prime.
+  // 5. Display the result in the U(DONE)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="card">
+          <h2>Prime Number Checker</h2>
+          <div className="input-container">
+            <input type="number" placeholder="Please, enter a number." />
+          </div>
+          <div className="button-group">
+            <button className="check-btn">Check</button>
+            <button className="clear-btn">Clear</button>
+          </div>
+          <p>It is a prime number!</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
